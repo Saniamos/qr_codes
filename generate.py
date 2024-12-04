c7db68f577d25454074a5fb8a52417f2977eb27b
Yale Hartmann <yale.hartmann@uni-bremen.de>
2024-12-04 16:00:42 +0100

    more qr codes

--- generate.py
+++ generate.py
@@ -4,11 +4,12 @@ from PIL import Image, ImageDraw, ImageFont
 import click
 
 @click.command()
-@click.option('--logo-path', default='g4g.jpg', help='Path to the logo image.')
-@click.option('--url', default='https://www.geeksforgeeks.org/', help='URL or text to encode in the QR code.')
-@click.option('--qr-color', default='Green', help='Color of the QR code.')
-@click.option('--output', default='gfg_QR.png', help='Output file name for the generated QR code.')
-def generate_qr(logo_path, url, qr_color, output):
+@click.option('--logo-path', default='logos/github/github-mark.png', help='Path to the logo image.')
+@click.option('--url', default='https://github.com/saniamos/', help='URL or text to encode in the QR code.')
+@click.option('--qr-color', default='#24292f', help='Color of the QR code.')
+@click.option('--output', default='codes/github.png', help='Output file name for the generated QR code.')
+@click.option('--include-url', is_flag=True, help='Include the URL text at the bottom of the QR code.')
+def generate_qr(logo_path, url, qr_color, output, include_url):
     # taking image which user wants in the QR code center
     logo = Image.open(logo_path)
 
@@ -39,9 +40,10 @@ def generate_qr(logo_path, url, qr_color, output):
     QRimg = QRcode.make_image(
         fill_color=qr_color, back_color="white").convert('RGB')
 
-    # calculate the size of the QR code's grid
-    qr_size = QRimg.size[0]
-    logo_size = int(qr_size * 0.25)  # logo should be 25% of the QR code size
+    # calc the amount of squares the image should take up
+    # overall the image should take up 25% and as it is centered, so that left and right of center are the same amounts covered
+    assert QRcode.modules_count % 2 == 1, "I'm assuming there is always a center row"
+    logo_size = (int(QRcode.modules_count * .125) * 2 + 1) * QRcode.box_size
 
     # resize logo_with_border to fit the QR code grid
     logo_with_border = logo_with_border.resize((logo_size, logo_size), Image.LANCZOS)
@@ -51,19 +53,21 @@ def generate_qr(logo_path, url, qr_color, output):
            (QRimg.size[1] - logo_with_border.size[1]) // 2)
     QRimg.paste(logo_with_border, pos)
 
-    # extend the canvas to add the URL at the bottom
-    new_height = QRimg.size[1] + 40  # Add space for the text
-    extended_img = Image.new('RGB', (QRimg.size[0], new_height), 'white')
-    extended_img.paste(QRimg, (0, 0))
+    if include_url:
+        # extend the canvas to add the URL at the bottom
+        new_height = QRimg.size[1] + 40  # Add space for the text
+        extended_img = Image.new('RGB', (QRimg.size[0], new_height), 'white')
+        extended_img.paste(QRimg, (0, 0))
 
-    # draw the URL text at the bottom
-    draw = ImageDraw.Draw(extended_img)
-    draw.text((QRimg.size[1] // 2, QRimg.size[1] - 10), url, fill=qr_color, font_size=15, anchor="mt")
+        # draw the URL text at the bottom
+        draw = ImageDraw.Draw(extended_img)
+        draw.text((QRimg.size[1] // 2, QRimg.size[1] - 10), url, fill=qr_color, font_size=15, anchor="mt")
+        QRimg = extended_img
 
     # save the QR code generated
-    extended_img.save(output)
+    QRimg.save(output)
 
-    print('QR code generated!')
+    print(f'Generated: {url}')
 
 if __name__ == '__main__':
     generate_qr()
