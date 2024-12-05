ba9bf9c12af6339197fe840867fe125206a6867b
Yale Hartmann <yale.hartmann@uni-bremen.de>
2024-12-05 13:58:09 +0100

    black codes

--- gen.sh
+++ gen.sh
@@ -1,8 +1,8 @@
-python3 generate.py --logo-path logos/github/github-mark.png --url "https://github.com/saniamos/" --qr-color "#24292f" --output codes/github.png
-python3 generate.py --logo-path logos/github/github-mark.png --url "https://github.com/saniamos/" --qr-color "#24292f" --output codes/github_url.png --include-url
+python3 generate.py --logo-path logos/github/github-mark.png --url "https://github.com/saniamos/" --qr-color "black" --output codes/github.png
+python3 generate.py --logo-path logos/github/github-mark.png --url "https://github.com/saniamos/" --qr-color "black" --output codes/github_url.png --include-url
 
-python3 generate.py --logo-path logos/LinkedIn/LI-In-Bug.png --url "https://linkedin.com/in/yale-hartmann/" --qr-color "#0a66c3" --output codes/linkedin.png 
-python3 generate.py --logo-path logos/LinkedIn/LI-In-Bug.png --url "https://linkedin.com/in/yale-hartmann/" --qr-color "#0a66c3" --output codes/linkedin_url.png --include-url
+python3 generate.py --logo-path logos/LinkedIn/LI-In-Bug.png --url "https://linkedin.com/in/yale-hartmann/" --qr-color "black" --output codes/linkedin.png 
+python3 generate.py --logo-path logos/LinkedIn/LI-In-Bug.png --url "https://linkedin.com/in/yale-hartmann/" --qr-color "black" --output codes/linkedin_url.png --include-url
 
-python3 generate.py --logo-path logos/researchgate/researchgate.jpeg --url "https://www.researchgate.net/profile/Yale-Hartmann" --qr-color "#01caac" --output codes/researchgate.png 
-python3 generate.py --logo-path logos/researchgate/researchgate.jpeg --url "https://www.researchgate.net/profile/Yale-Hartmann" --qr-color "#01caac" --output codes/researchgate_url.png --include-url
+python3 generate.py --logo-path logos/researchgate/researchgate.jpeg --url "https://researchgate.net/profile/Yale-Hartmann" --qr-color "black" --output codes/researchgate.png 
+python3 generate.py --logo-path logos/researchgate/researchgate.jpeg --url "https://researchgate.net/profile/Yale-Hartmann" --qr-color "black" --output codes/researchgate_url.png --include-url
